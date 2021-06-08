8b38c836b547384a1cb3287fe69c51c7fef7db8f
neeko <girl2learn2code@gmail.com>
2021-06-08 06:26:03 -0700

    trying to print numbers

--- math.py
+++ math.py
@@ -1,6 +1,9 @@
+# imports go here i guess
+
+
+
 # test 1/2
 print('code starts here')
-
 
 class Addition:
     def __init__(self, a, b):
@@ -9,16 +12,17 @@ class Addition:
     def sum(self):
         return self.a + self.b
 
-c = Addition(10000, 2)
+c = Addition(2, 2)
+
 c.sum() 
+
+# print(self.a)
 print(Addition.sum(c))
-
-
-
-# test 2/2
-print('code ended')
 
 
 # I want to fit all the operations into this list but right now I will focus on addition, i would normally try using functions but I read about Classes so today so I want to try and implement that here and if I get stuck mayb u can help
 operations = []
 pass
+
+# test 2/2
+print('code ended')
