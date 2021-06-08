a33d807042809333441952d124bb8e7d7be345e4
neeko <girl2learn2code@gmail.com>
2021-06-08 06:49:19 -0700

    ADDED GHETTO DISPLAY FUNCTION LOL

--- math.py
+++ math.py
@@ -12,12 +12,18 @@ class Addition:
     def sum(self):
         return self.a + self.b
 
+    def displayfunc(z):
+        print(z.a , z.b, "=")
+ 
 c = Addition(2, 2)
 
 c.sum() 
 
-# print(self.a)
+
+c.displayfunc()
 print(Addition.sum(c))
+
+
 
 
 # I want to fit all the operations into this list but right now I will focus on addition, i would normally try using functions but I read about Classes so today so I want to try and implement that here and if I get stuck mayb u can help
