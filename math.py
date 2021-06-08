e54ec0e408f26812a83d6f5aa672aaef602aaac1
neeko <neekohlahdc@gmail.com>
2021-06-08 02:09:06 -0700

    Made first function and made comments on ideas on how to proceed.

---
+++ math.py
@@ -0,0 +1,24 @@
+# Addition
+def add(num1, num2):
+    pass
+
+add()
+
+
+
+
+
+# I want to fit all the operations into this list but right now I will focus on addition
+# operations = []
+
+
+
+
+
+# def testfunction():
+#     return add()
+
+
+
+
+
