e805bf7033840e17df4b43fa31fa0e8992ac4079
neeko <neekohlahdc@gmail.com>
2021-06-08 04:51:02 -0700

    currently stuck lol, but after i figure this out it should be pretty easy to implement it for the other operations.

--- math.py
+++ math.py
@@ -1,29 +1,45 @@
 # Addition
-def add(num1, num2):
-    return add 
- 
+# def add(a, b):
+#     sum=a+b
+#     return sum
 
 
-print(add(1,2))
+# test 1/2
+print('code starts here')
 
 
-def sum();
+class Addition:
+    def __init__(self, a, b, sum):
+        self.a = a
+        self.b = b
+
+    def sum(self):
+        # idk what to put here since i cant get a or b
+        return self.sum 
+
+
+add00 = Addition(11, 1)
+add01 = Addition(10, 5)
+# add2 = Addition(4)
+print(add00.a)
+print(add00.b)
+print(add00.sum)
+print(add01.a)
+
+# print(add2.num)
+
+
+# might need a function for sum
+
+
+def sum():
+    pass
+
+
+# test 2/2
+print('code ended')
+
+
+# I want to fit all the operations into this list but right now I will focus on addition, i would normally try using functions but I read about Classes so today so I want to try and implement that here and if I get stuck mayb u can help
+operations = []
 pass
-
-
-
-
-# I want to fit all the operations into this list but right now I will focus on addition
-# operations = []
-
-
-
-
-
-# def testfunction():
-#     return add()
-
-
-
-
-
